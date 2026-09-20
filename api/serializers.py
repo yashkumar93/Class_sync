7c4fb3dfe960e78a35bb88b16a9bf5aeb3fba9f6
yashkumar93 <charmingyashrock@gmail.com>
2026-09-20 11:02:00 +0530

    Fix HTTP 400 on login by removing premature local authenticate in LoginSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -19,17 +19,9 @@ from notifications.models import Notification, DeviceToken, RiskFlag, Announceme
 # ---------------------------------------------------------------------------
 
 
-from django.contrib.auth import authenticate
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-
-    def validate(self, data):
-        user = authenticate(username=data['username'], password=data['password'])
-        if not user:
-            raise serializers.ValidationError('Invalid username or password.')
-        data['user'] = user
-        return data
+    username = serializers.CharField(required=True, allow_blank=False)
+    password = serializers.CharField(required=True, allow_blank=False, write_only=True)
 
 
 # ---------------------------------------------------------------------------
